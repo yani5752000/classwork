f35678b6a2ef074188cd17d45e1460f56d996b4c
Farid Asadpour <asadpourfarid@gmail.com>
2022-05-09 16:03:56 -0400

    variables1

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/variables/AllAboutMe.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.foundations.variables;
+
+/**
+ *
+ * @author faridasadpour
+ */
+public class AllAboutMe {
+    public static void main(String[] args){
+        System.out.println("My name is Ada Lovelace.");
+        System.out.println("My favorite food is hot buttered gnocchi.");
+        System.out.println("I have 5 pets.");
+        System.out.println("I live in a pineapple under the sea, and I love it here.");
+        System.out.println("It is true I know how to whistle.");
+    }
+}
---
+++ PracticeProgramming/src/main/java/com/sg/foundations/variables/MenuOfChampions.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.foundations.variables;
+
+/**
+ *
+ * @author faridasadpour
+ */
+public class MenuOfChampions {
+    public static void main(String[] args){
+        System.out.println(".oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.");
+        System.out.println("");
+        System.out.println("            WELCOME TO RESTAURANT NIGHT VALE!");
+        System.out.println("            Today's Menu Is...");
+        System.out.println("");
+        System.out.println(".oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.");
+        System.out.println("");
+        System.out.println("             $ 500.00 *** Slice of Big Rico Pizza");
+        System.out.println("             $ 2.00 ***** Invisible Strawberry Pie");
+        System.out.println("             $ 1.50 ***** Denver Omelet");
+    }
+}
