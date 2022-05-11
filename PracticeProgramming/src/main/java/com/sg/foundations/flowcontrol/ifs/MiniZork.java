bb53e7d7e8dccabca2cf7a3dbc470b844fb5b750
Farid Asadpour <asadpourfarid@gmail.com>
2022-05-11 15:46:24 -0400

    ifs

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/ifs/MiniZork.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.foundations.flowcontrol.ifs;
+
+/**
+ *
+ * @author faridasadpour
+ */
+import java.util.Scanner;
+
+public class MiniZork {
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("You are standing in an open field west of a white house, there is a deep hole too");
+        System.out.println("With a boarded front door.");
+        System.out.println("There is a small mailbox here.");
+        System.out.print("Go to the house, or open the mailbox, or go into the hole? ");
+
+        String action = userInput.nextLine();
+
+        if (action.equals("open the mailbox")) {
+            System.out.println("You open the mailbox.");
+            System.out.println("It's really dark in there.");
+            System.out.print("Look inside or stick your hand in? ");
+            action = userInput.nextLine();
+
+            if (action.equals("look inside")) {
+                System.out.println("You peer inside the mailbox.");
+                System.out.println("It's really very dark. So ... so very dark.");
+                System.out.print("Run away or keep looking? ");
+                action = userInput.nextLine();
+
+                if (action.equals("keep looking")) {
+                    System.out.println("Turns out, hanging out around dark places isn't a good idea.");
+                    System.out.println("You've been eaten by a grue.");
+                } else if (action.equals("run away")) {
+                    System.out.println("You run away screaming across the fields - looking very foolish.");
+                    System.out.println("But you alive. Possibly a wise choice.");
+                }
+            } else if (action.equals("stick your hand in")) { System.out.println("Oh God");}
+        } else if (action.equals("go to the house")) { System.out.print("whistle or open the mail box?");
+                action = userInput.nextLine();}
+        else if (action.equals("go into the hole")) { }
+    }
+}
