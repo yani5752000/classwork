d7804ee834c6f6fbdde3c76270b5fd0dfab9a960
Farid Asadpour <asadpourfarid@gmail.com>
2022-09-17 17:42:40 -0400

    react32

--- react-dvdlibrary/src/App.js
+++ react-dvdlibrary/src/App.js
@@ -9,29 +9,57 @@ import EditModal from './components/EditModal';
 
 import { Container, Row, Col } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
-function App() {
-  return (
-    <Container fluid>
-      <Row>
-        <Col sm={4}>
-          <CreateButton />
-        </Col>
-        <Col sm={8}>
-          <SerachForm />
-        </Col>
-      </Row>
-      <hr />
-      <Row>
-        <Col className="text-center">
-          <DVDTable />
-        </Col>
-      </Row>
-      <hr />
-      {/* <CreateModal /> */}
-      {/* <EditModal /> */}
-      {/* <DeleteModal /> */}
-    </Container>
-  );
+
+const SERVICE_URL = "http://dvd-library.us-east-1.elasticbeanstalk.com/"
+
+class App extends React.Component {
+
+  state = {
+    loading: false,
+    dvdData: [
+      {
+        "title": "hi0",
+        "releaseYear": "2010",
+        "director": "Janesen",
+        "rating": "PG-10"
+      }]
+  }
+
+  componentDidMount() {
+    console.log("App is now mounted.")
+    this.setState({ loading: true })
+    console.log("Loading dvd data")
+    fetch(SERVICE_URL + "/dvds")
+      .then(data => data.json())
+      .then(data => this.setState(
+        { dvdData: data, loading: false }
+      ))
+  }
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col sm={4}>
+            <CreateButton />
+          </Col>
+          <Col sm={8}>
+            <SerachForm />
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col className="text-center">
+            <DVDTable dvds={this.state.dvdData} />
+          </Col>
+        </Row>
+        <hr />
+        {/* <CreateModal /> */}
+        {/* <EditModal /> */}
+        {/* <DeleteModal /> */}
+      </Container>
+    );
+  }
 }
 
 export default App;
--- react-dvdlibrary/src/components/DVDTable.js
+++ react-dvdlibrary/src/components/DVDTable.js
@@ -1,7 +1,65 @@
 import React from 'react';
+import { Table, Button } from 'react-bootstrap'
 
-function DVDTable (){
-  return <h1>DVD Table</h1>
+const DVDTableHeader = () => {
+  return (
+      <tr>
+          <th>Title</th>
+          <th>Release Year</th>
+          <th>Director</th>
+          <th>Rating</th>
+          <th></th>
+          <th></th>
+      </tr>
+  );
+}
+
+const DVDTableRow = ({ dvd }) => {
+  return (
+      <tr>
+          <td>{dvd.title}</td>
+          <td>{dvd.releaseYear}</td>
+          <td>{dvd.director}</td>
+          <td>{dvd.rating}</td>
+          <td><Button>Edit</Button></td>
+          <td><Button>Delete</Button></td>
+      </tr>
+  );
+}
+
+class DVDTable extends React.Component {
+  // this static property will initialize a prop with data
+    // if it hasn't been provided by the parent component
+    static defaultProps = {
+        dvds: [
+            {
+                "title": "hi1",
+                "releaseYear": "2014",
+                "director": "Jones",
+                "rating": "PG"
+            },
+            {
+              "title": "hi2",
+              "releaseYear": "2012",
+              "director": "Janes",
+              "rating": "PG-13"
+            }
+        ]
+    }
+
+
+  render() {
+      return (<Table striped bordered responsive>
+          <thead>
+              <DVDTableHeader />
+          </thead>
+          <tbody>
+              {this.props.dvds.map((dvd, i) => {
+                  return <DVDTableRow dvd={dvd} key={i} />
+              })}
+          </tbody>
+      </Table>)
+  }
 }
 
 export default DVDTable
