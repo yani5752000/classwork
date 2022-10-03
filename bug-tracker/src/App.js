55195494aa3aaeed21a063151463c04d10985043
Farid Asadpour <asadpourfarid@gmail.com>
2022-10-03 18:01:41 -0400

    react45

--- bug-tracker/src/App.js
+++ bug-tracker/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import SubmitFormHtml from "./components/SubmitFormHtml";
+import SetStateExample from "./components/SetStateExample";
  
 class App extends Component {
   render() {
     return (
       <div className="container">
         <h3>React Form - HTML</h3>
+        <SetStateExample></SetStateExample>
         <SubmitFormHtml />
         <br />
       </div>
