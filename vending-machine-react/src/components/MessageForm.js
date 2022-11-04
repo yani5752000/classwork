b67662bcf33416fa866b4e4e8b6e276628d7b7a8
Farid Asadpour <asadpourfarid@gmail.com>
2022-11-04 17:38:57 -0400

    react73

--- vending-machine-react/src/components/MessageForm.js
+++ vending-machine-react/src/components/MessageForm.js
@@ -16,6 +16,9 @@ class MessageForm extends React.Component {
         return (
             <Form>
                 <h4 className="text-center">Messages</h4>
+                <div className="text-center square border border-secondary" style={{height: "40px"}}>
+                            {message}
+                        </div>
                 <Row>
                     <Col sm={4}  className="text-right">Item: </Col>
                     <Col sm={8} className='row'>
