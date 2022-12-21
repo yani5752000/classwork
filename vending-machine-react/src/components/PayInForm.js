67cab21bf5a683e9ece9add55424b3b5fa8ae9dd
Farid Asadpour <asadpourfarid@gmail.com>
2022-12-21 17:40:36 -0500

    react105

--- vending-machine-react/src/components/PayInForm.js
+++ vending-machine-react/src/components/PayInForm.js
@@ -35,7 +35,7 @@ class PayInForm extends React.Component {
                 </div>
                 {/* <br /> */}
                 <div className="text-center">
-                    {/* and the button for depositing one dime */}
+                    {/* and the button for depositing one dime coin */}
                   <Button variant="outline-secondary" name="dime" onClick={addToDeposit}>
                       Add Dime
                   </Button>
