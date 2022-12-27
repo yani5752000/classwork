3deba37fd2a00fbf5ff1fc09af203f269d8dbfc6
Farid Asadpour <asadpourfarid@gmail.com>
2022-12-27 17:16:55 -0500

    react109

--- vending-machine-react/src/components/PayInForm.js
+++ vending-machine-react/src/components/PayInForm.js
@@ -28,7 +28,7 @@ class PayInForm extends React.Component {
                   <Button variant="outline-secondary" name="dollar" onClick={addToDeposit}>
                       Add Dollar
                   </Button>
-                  {/* and the button for depositing one quarter coin*/}
+                  {/* and here the button for depositing one quarter coin*/}
                   <Button variant="outline-secondary" name="quarter" onClick={addToDeposit}>
                       Add Quarter
                   </Button>
