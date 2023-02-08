7b1102325711ef07d6487d233d35a3e884df196d
Farid Asadpour <asadpourfarid@gmail.com>
2023-02-08 16:00:45 -0500

    react140

--- vending-machine-react/src/App.js
+++ vending-machine-react/src/App.js
@@ -93,7 +93,7 @@ class App extends React.Component {
     });
     
   }
-//function for purchasing
+//function for purchasing items
   makePurchase = (event) => {
     if (!this.state.itemNumber) {
       this.setState({message: "Please make a selection"});
