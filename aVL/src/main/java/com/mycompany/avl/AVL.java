4c1ae3865ebc219fa91baa90384084ab03ef7aad
Farid Asadpour <asadpourfarid@gmail.com>
2023-04-21 16:55:39 -0400

    SS29

--- aVL/src/main/java/com/mycompany/avl/AVL.java
+++ aVL/src/main/java/com/mycompany/avl/AVL.java
@@ -13,21 +13,36 @@ public class AVL {
     Node root;
     class Node{
         int item;
-        int height;
+        int balance = balance();
         Node left, right;
         
         public Node(int key){
             item = key;
-            height = 0;
             left = null;
             right = null;
         }
+        int height(){
+            if(right == null && left == null){
+                return 0;
+            }else if(right == null){
+                return 1 + left.height();
+            }else if(left ==  null){
+                return 1 + right.height();
+            }else {
+                return 1 + Math.max(right.height(), left.height());
+            }
+        }
         
         int balance(){
-            if(height == 0){
+            if(right == null && left == null){
                 return 0;
+            }else if(right == null){
+                return left.height();
+            }else if(left ==  null){
+                return -right.height();
+            }else {
+                return left.height() - right.height();
             }
-            return left.height - right.height;
         }
     }
     
@@ -39,67 +54,73 @@ public class AVL {
         if(root.left == null && root.right == null){
             if(number < root.item){
                 Node left = new Node(number);
-                left.balance = 0;
                 root.left = left;
-                root.balance++;
-                root.height++;
                 return;
             } else {
                 Node right = new Node(number);
                 root.right = right;
-                root.balance = -1;
-                root.height++;
                 return;
             }
         }
-        if(root.left == null && root.right.right == null && root.right.left == null){
+        if(root.left == null && root.height() == 1){
             if(number < root.item ){
                 Node left = new Node(number);
                 root.left = left;
-                root.balance++;
                 return;
             }else if(number < root.right.item) {
                 Node left = new Node(root.item);
                 root.left = left;
                 root.item = number;
-                root.balance++;
                 return;
             } else {
                 Node left = new Node(root.item);
                 root.left = left;
                 root.item = root.right.item;
-                root.balance++;
                 root.right.item = number;
                 return;
             }
         }
-        if(root.right == null && root.left.left == null && root.left.right == null){
+        if(root.right == null && root.height() == 1){
             if(number > root.item ){
                 Node right = new Node(number);
                 root.right = right;
-                root.balance--;
                 return;
             }else if(number > root.left.item) {
                 Node right = new Node(root.item);
                 root.right = right;
                 root.item = number;
-                root.balance--;
                 return;
             } else {
                 Node right = new Node(root.item);
-                right.balance = 0;
                 root.right = right;
                 root.item = root.left.item;
-                root.balance--;
                 root.left.item = number;
                 return;
             }
         }
-        if(root.balance == 0 && number > root.item){
-            AVL a = new AVL();
-            a.root = root.right;
-            a.insert(number);
-            
+        if(root.balance == 0){
+            if(number > root.item){
+                AVL a = new AVL();
+                a.root = root.right;
+                a.insert(number);
+                return;
+            }else {
+                AVL a = new AVL();
+                a.root = root.left;
+                a.insert(number);
+            }
+        } else if(root.balance == -1){
+            if(number < root.item){
+                AVL a = new AVL();
+                a.root = root.left;
+                a.insert(number);
+            }else if(number < root.right.item){
+                int temp = root.item;
+                root.item = number;
+                AVL a = new AVL();
+                a.root = root.left;
+                a.insert(temp);
+            }
         }
     }
 }
